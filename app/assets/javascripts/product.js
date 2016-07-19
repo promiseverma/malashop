bd3e0d183707e27d64e220d788b2e91af834688b
Linchpin <you@example.com>
2016-07-19 18:07:05 +0530

    bug fixes

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,8 +1,8 @@
 function callExecuter(click){ 
-  $('#item-content').append('<br><br><input type="text" name="product[items_attributes]['+click+'][item_name]" id="product_items_attributes_'+click+'_item_name" class="form-control">');
+  $('#item-content').append('<input type="text" name="product[items_attributes]['+click+'][item_name]" id="product_items_attributes_'+click+'_item_name" class="form-control">');
   }
-function delExecuter(click){ 
-  $('#item-content  div:last').remove();
+function delExecuter(){ 
+  $('#item-content input:last').remove();
   }
 
 
