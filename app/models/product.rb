db9882ae802097e940a80bc159d784e6add66985
promiseverma <promiseverma5@gmail.com>
2017-01-21 21:58:36 +0530

    added destroy on production

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :items, :dependent => :destroy
   has_many :images, :dependent => :destroy
-  has_many :prices
-  has_many :materials, through: :prices
+  has_many :prices, :dependent => :destroy
+  has_many :materials, through: :prices, :dependent => :destroy
   # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   accepts_nested_attributes_for :items
