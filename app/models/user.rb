3608bc7ae92366f633486da147711cd6ba2849c3
promiseverma <promise.verma@vvdntech.in>
2018-05-19 12:05:42 +0530

    Remove signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :validatable, :rememberable, :registerable,:recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :validatable, :rememberable, :trackable, :validatable
 end
